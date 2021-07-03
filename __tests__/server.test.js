aeb2a68b6d7f84136acba7a1dc16cf911fde4261
Mohammed-Alramahi <vittosc1997@gmail.com>
2021-07-03 21:19:29 +0300

    added status 200 and 500 to test

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -8,4 +8,14 @@ describe("SERVER TEST:", () => {
       expect(data.status).toBe(404);
     });
   });
+  it("should respond with a 200 success", async () => {
+    return mockRequest.get("/person?name=ramahi").then((data) => {
+      expect(data.status).toBe(200);
+    });
+  });
+  it("should respond with a 500 on error", async () => {
+    return mockRequest.get("/person").then((data) => {
+      expect(data.status).toBe(500);
+    });
+  });
 });
